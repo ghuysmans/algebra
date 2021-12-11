55a7061c91689269ce93619cde139c2cc6d35754
Guillaume Huysmans <ghuysmans99@gmail.com>
2021-12-11 13:58:50 +0100

    Reimplement fastpow in Semigroup

--- lib/monoid.ml
+++ lib/monoid.ml
@@ -26,24 +26,14 @@ end
 
 module Tools = struct
   module S (M : S) = struct
+    module T = Semigroup.Tools.S (M)
     open M
 
     let fastpow a ~b =
-      let rec f a b r =
-        if b = 0 then
-          (* a^0 * r = 1 * r = r *)
-          r
-        else if b (* is even *) land 1 = 0 then
-          (* a^(2k) = (a^2)^k *)
-          f (a @ a) (b / 2) r
-        else
-          (* a^(2k+1) = a * a^(2k) = a * (a^2)^c *)
-          f (a @ a) (b / 2) (r @ a)
-      in
-      if b < 0 then
-        raise (Invalid_argument "negative exponent")
+      if b = 0 then
+        id
       else
-        f a b id
+        T.fastpow a ~b
 
     let order t =
       let rec f u n =
@@ -56,24 +46,14 @@ module Tools = struct
   end
 
   module I (M : I) = struct
+    module T = Semigroup.Tools.I (M)
     open M
 
     let fastpow a ~b =
-      let rec f a b r =
-        if b = 0 then
-          (* a^0 * r = 1 * r = r *)
-          r
-        else if b (* is even *) land 1 = 0 then
-          (* a^(2k) = (a^2)^k *)
-          f (a @ a) (b / 2) r
-        else
-          (* a^(2k+1) = a * a^(2k) = a * (a^2)^c *)
-          f (a @ a) (b / 2) (r @ a)
-      in
-      if b < 0 then
-        raise (Invalid_argument "negative exponent")
+      if b = 0 then
+        make ()
       else
-        f a b (make ())
+        T.fastpow a ~b
 
     let order t =
       let id = make () in
--- lib/semigroup.ml
+++ lib/semigroup.ml
@@ -25,3 +25,69 @@ module Direct_product = struct
     let copy (a, b) = A.copy a, B.copy b
   end
 end
+
+module Tools = struct
+  module S (M : S) = struct
+    open M
+
+    let fastpow a ~b =
+      let rec f a b r =
+        if b = 0 then
+          (* a^0 * r = 1 * r = r *)
+          r
+        else if b (* is even *) land 1 = 0 then
+          (* a^(2k) = (a^2)^k *)
+          f (a @ a) (b / 2) r
+        else
+          (* a^(2k+1) = a * a^(2k) = a * (a^2)^c *)
+          f (a @ a) (b / 2) (r @ a)
+      in
+      if b < 0 then
+        raise (Invalid_argument "negative exponent")
+      else if b = 0 then
+        raise (Invalid_argument "unknown identity")
+      else
+        f a (b - 1) a
+
+    let order t =
+      let rec f u n =
+        if equal u t then
+          n - 1
+        else
+          f (t @ u) (n + 1)
+      in
+      f t 0
+  end
+
+  module I (M : I) = struct
+    open M
+
+    let fastpow a ~b =
+      let rec f a b r =
+        if b = 0 then
+          (* a^0 * r = 1 * r = r *)
+          r
+        else if b (* is even *) land 1 = 0 then
+          (* a^(2k) = (a^2)^k *)
+          f (a @ a) (b / 2) r
+        else
+          (* a^(2k+1) = a * a^(2k) = a * (a^2)^c *)
+          f (a @ a) (b / 2) (r @ a)
+      in
+      if b < 0 then
+        raise (Invalid_argument "negative exponent")
+      else if b = 0 then
+        raise (Invalid_argument "unknown identity")
+      else
+        f a (b - 1) a
+
+    let order t =
+      let rec f u n =
+        if equal u t then
+          n - 1
+        else
+          f (t @ u) (n + 1)
+      in
+      f t 0
+  end
+end
